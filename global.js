98d9afa3808973a039722ee1ae9c96c032fea272
iam_nevets <33188789+iamnevets@users.noreply.github.com>
2024-06-10 17:40:05 +0000

    Update global.js

--- global.js
+++ global.js
@@ -1,14 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const currentUrl = window.location.pathname.split("/").pop();
-    const navLinks = document.querySelectorAll(".nav-link");
+document.addEventListener("DOMContentLoaded", function () {
+  const currentUrl = window.location.pathname.split("/").pop();
+  const navLinks = document.querySelectorAll(".nav-link");
 
-    // Loop through each navigation link
-    navLinks.forEach(function(link) {
-        const linkUrl = link.getAttribute("href").split("/").pop();
-        if (linkUrl === currentUrl) {
-            link.classList.add("current-page");
-        } else {
-            link.classList.remove("current-page")
-        }
+  const menuBtn = document.getElementById("menu-btn");
+  const mobileMenu = document.getElementById("mobile-menu");
+  const mobileMenuItems = document.querySelectorAll(".mobile-menu-item");
+
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
+
+  mobileMenuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      mobileMenu.classList.add("hidden");
     });
+  });
+
+  navLinks.forEach(function (link) {
+    const linkUrl = link.getAttribute("href").split("/").pop();
+    if (linkUrl === currentUrl) {
+      link.classList.add("current-page");
+    } else {
+      link.classList.remove("current-page");
+    }
+  });
 });
